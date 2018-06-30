f6c45ff48db3c064e5597ad56e6451addde1c354
Kianna <kianna@Allans-MacBook-Pro-4.local>
2018-06-30 10:32:49 -0700

    started stress testing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,37 @@
-const app = require("./app");
+const cluster = require("cluster");
 
-const port = 3002;
+if (cluster.isMaster) {
+	let numWorkers = require("os").cpus().length;
+	console.log("Master cluster setting up " + numWorkers + " workers...");
+	for (let i = 0; i < numWorkers; i += 1) {
+		cluster.fork();
+	}
 
-app.listen(port, () => {
-	console.log("listening on port 3002");
-});
+	cluster.on("online", worker => {
+		console.log("Worker " + worker.process.pid + " is online");
+	});
+
+	cluster.on("exit", (worker, code, signal) => {
+		console.log(
+			"Worker " +
+				worker.process.pid +
+				" died with code: " +
+				code +
+				", and signal: " +
+				signal
+		);
+		console.log("Starting a new worker");
+		cluster.fork();
+	});
+} else {
+	const app = require("./app");
+	app.all("/*", (req, res) => {
+		res.send("process " + process.pid + " says hello!").end();
+	});
+
+	const server = app.listen(3002, () => {
+		console.log(
+			"Process " + process.pid + " is listening to all incoming requests"
+		);
+	});
+}
